e97df99d35ef8695fd8f45429019fe608441940d
Bestian Tang <bestian@gmail.com>
2020-04-27 07:08:09 +0800

    deploy

---
+++ precache-manifest.6bd9a5794d420d15c93764fc20131fa7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b8a3bb379b4f024fb2",
+    "url": "/zhfixer/css/app.aa193da7.css"
+  },
+  {
+    "revision": "1d4ef1fcfba1b4b3cd807963c42d40a8",
+    "url": "/zhfixer/diffZhCn-ZhTw.txt"
+  },
+  {
+    "revision": "658fde8e34c6d08d4155e2fa80c60dc3",
+    "url": "/zhfixer/diffZhTw-ZhTw.txt"
+  },
+  {
+    "revision": "47bca9fbd71851285075c8c38d60a038",
+    "url": "/zhfixer/index.html"
+  },
+  {
+    "revision": "a5b8a3bb379b4f024fb2",
+    "url": "/zhfixer/js/app.66897d24.js"
+  },
+  {
+    "revision": "4f64bfe1d19e3c6d27b9",
+    "url": "/zhfixer/js/chunk-vendors.890f9cbb.js"
+  },
+  {
+    "revision": "b615d5bddf1454752e5621829e6fb7e6",
+    "url": "/zhfixer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/zhfixer/robots.txt"
+  },
+  {
+    "revision": "0c6e0ed4cf4c3b1ad7d0460d0dd1eac6",
+    "url": "/zhfixer/trans.json"
+  },
+  {
+    "revision": "ee8cb45a0c3251961df0b3298eef93e0",
+    "url": "/zhfixer/trans.txt"
+  }
+]);
